835adae7b73cc5bb11026524494ad835116fab53
Jonathan Barney <jb@jonathanbarney.com>
2019-03-11 17:22:02 -0700

    Rebuilt with assets

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   API_URL: "https://protium.jonathanbarney.com/v1/",
-  SITE_URL: "https://tritium.elichor.com/",
+  SITE_URL: "https://protium.jonathanbarney.com/",
 };
